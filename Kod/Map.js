12b9c430fd6ab272ae7c66e7c05012a93edb18c1
Marike Grinde <mg222cd@student.lnu.se>
2016-05-04 13:53:19 +0200

    feat: hårdkokad markör på karta

--- Kod/Map.js
+++ Kod/Map.js
@@ -1,11 +1,76 @@
+"use strict";
+
 function initialize () {
+    var myLatLng = {lat: 62.7013, lng: 12.38913};
+
     var mapOptions = {
-        center: new google.maps.LatLng(59.999999, 14.9999999),
-        zoom: 2
+        center: new google.maps.LatLng(62.7013, 12.38913),
+        zoom: 8
     };
     
-    map = new google.maps.Map(document.getElementById('map-canvas'),
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
+    var marker = new google.maps.Marker({
+        position : myLatLng,
+        map : map
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+/*
+//Constructor
+var Map = function(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.infoWindow = undefined;
+    this.markers = [];
+    var mapOptions = {
+        center: new google.maps.LatLng (this.latitude, this.longitude),
+    }
+    this.map = new google.maps.Map (document.getElementById('map-canvas'), mapOptions);
+};
+*/
+
+
+/*
+
+function initialize() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
+
+Map.prototype.setMarker = function(location) {
+    var that = this;
+    var latLng = new google.maps.LatLng (location.latitude, location.longitude);
+    var marker = new google.maps.Marker ({
+        position: latLng,
+        map: this.map,
+    });
+    this.markers.push(marker);
+    google.maps.event.addListener(marker, 'click', function () {
+        that.getInfoWindow(location, marker);
+    });
+};
+
+
+Map.prototype.deleteMarkers = function() {
+    for (var i = 0; i < this.markers.length; i++) {
+        this.markers[i].setMap(null);
+    }
+    this.markers = [];
+}
+
+*/
