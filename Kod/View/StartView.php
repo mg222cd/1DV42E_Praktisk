cc890fe2fa9bcf2759cf3919f5b0b49f9d10da55
Marike Grinde <mg222cd@student.lnu.se>
2016-05-27 12:37:28 +0200

    fix: startsidan färdigställd

--- Kod/View/StartView.php
+++ Kod/View/StartView.php
@@ -8,19 +8,19 @@ class StartView{
 		$html = '
 			<div class="row">
 		    <div class="col-xs-12 col-sm-10">
-		        <h1>Väderprognoser från</h1>
+		        <h1>
+		        	Väderprognoser från
 		        	<a href="http://www.yr.no">
-					<img class="startpagePic" src="https://upload.wikimedia.org/wikipedia/commons/4/48/Yr-logo.png" alt="logo yr" title="logo yr">
+					<img class="startpagePic startLogos" src="https://upload.wikimedia.org/wikipedia/commons/4/48/Yr-logo.png" alt="logo yr" title="logo yr">
 					</a>
+					och
 					<a href="http://www.smhi.se">
-					<img class="smhi" src="http://www.smhi.se/polopoly_fs/1.1108.1398236874!/image/SMHIlogo.png_gen/derivatives/Original/SMHIlogo.png" alt="logo smhi" title="logo smhi">
+					<img class="startLogos" src="http://www.smhi.se/polopoly_fs/1.1108.1398236874!/image/SMHIlogo.png_gen/derivatives/Original/SMHIlogo.png" alt="logo smhi" title="logo smhi">
 					</a>
-		        <p>
+				</h1>
+		        <h2 class="darkblueAsInHeader">
 		        Senaste sökningarna:
-		        </p>
-		        <p>
-		        bla bla bla bla
-		        </p>
+		        </h2>
 		    </div>
 		    </div>
 		';
